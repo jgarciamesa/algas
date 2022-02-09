7bc31a554a5bd1f9ceeb4bd3395c6b74643a9c51
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2022-02-09 16:02:38 -0700

    Fix calculate frequency

--- scripts/plot_histogram.R
+++ scripts/plot_histogram.R
@@ -1,6 +1,7 @@
 
 basic_plot = function(counts_file) {
 	counts = read.csv(file = counts_file, header = FALSE)
+	counts$V2 = counts$V2/sum(counts$V2)
 	pdf(file = paste0("freq_plot_", basename(counts_file), ".pdf"))
 	plot(counts, type = 'p', main = 'Gap Size Frequency', xlab = 'Gap lengths', ylab = 'Frequency')
 	dev.off()
