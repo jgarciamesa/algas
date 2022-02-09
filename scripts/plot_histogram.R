ad47b85ef5fcb8aaa7308b9bf7f05c3b62fa640f
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2022-02-09 16:00:53 -0700

    Change plot to show frequencies

--- scripts/plot_histogram.R
+++ scripts/plot_histogram.R
@@ -1,8 +1,8 @@
 
 basic_plot = function(counts_file) {
 	counts = read.csv(file = counts_file, header = FALSE)
-	pdf(file = "gap_histogram.pdf")
-	plot(counts, type = 'h', main = 'Gap histogram', xlab = 'Gap lengths', ylab = 'Frequency')
+	pdf(file = paste0("freq_plot_", basename(counts_file), ".pdf"))
+	plot(counts, type = 'p', main = 'Gap Size Frequency', xlab = 'Gap lengths', ylab = 'Frequency')
 	dev.off()
 }
 
