8746478e4c22fd825336074b8c1c3d562bfc4e73
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2022-04-06 13:44:00 -0700

    Now histogram handles multiple fasta inputs

--- algas.cpp
+++ algas.cpp
@@ -17,34 +17,44 @@ int main(int argc, char* const argv[]) {
     // histogram
     if(strcmp(argv[1], "histogram") == 0) {
         if((argc < 3) || (strcmp(argv[2], "help") == 0)) {
-            std::cout << "Usage:    algas histogram fasta" << std::endl;
+            std::cout << "Usage:    algas histogram fasta(s)" << std::endl;
+            return EXIT_SUCCESS;
         }
-        // read fasta file
-        algas::data_t data = algas::fasta::read_fasta(argv[2]);
 
         // gap counts vector
         //  each position is the number of gaps with its length (e.g. value at
         //  position 1 is number of gaps of size 1).
         //  assuming no sequence is entirely formed of gaps, we consider gap
         //  lengths from 0 to (longest seq size-1)
-        std::vector<size_t> counts(data.len());
+        std::vector<size_t> counts;
 
-        // find gaps on each sequence
-        for(const std::string& seq : data.seqs) {
-            size_t pos{seq.find(GAP, 0)};
-            while(pos != std::string::npos) {
-                size_t count{1};
-                // add current gap count until a nucleotide is found
-                do {
-                    pos++;
-                    count++;
-                    if(pos + 1 >= seq.size()) {
-                        break;
-                    }
-                } while(seq.at(pos + 1) == GAP);
-                counts[count]++;
-                // look for next gap
-                pos = seq.find(GAP, pos + 1);
+        // for each fasta file in input
+        for(int file = 2; file < argc; ++file) {
+            // read fasta file
+            algas::data_t data = algas::fasta::read_fasta(argv[file]);
+
+            // if capacity of vector is smaller than size of aln, resize
+            if(counts.capacity() < data.len()) {
+                counts.resize(data.len());
+            }
+
+            // find gaps on each sequence
+            for(const std::string& seq : data.seqs) {
+                size_t pos{seq.find(GAP, 0)};
+                while(pos != std::string::npos) {
+                    size_t count{1};
+                    // add current gap count until a nucleotide is found
+                    do {
+                        pos++;
+                        count++;
+                        if(pos + 1 >= seq.size()) {
+                            break;
+                        }
+                    } while(seq.at(pos + 1) == GAP);
+                    counts[count]++;
+                    // look for next gap
+                    pos = seq.find(GAP, pos + 1);
+                }
             }
         }
 
